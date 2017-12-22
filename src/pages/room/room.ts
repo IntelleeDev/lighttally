a89070e25fdc191fb6c25c2d6a9cd8361791638d
mikeysweede <mikeysweede@gmail.com>
2017-12-22 20:38:00 +0100

    New commit

--- src/pages/room/room.ts
+++ src/pages/room/room.ts
@@ -1,6 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
 import { Slides, IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { DashboardPage } from '../dashboard/dashboard';
+import { ReplacementComponent } from '../../components/replacement/replacement';
+import { ExistingLightComponent } from '../../components/existing-light/existing-light';
+import { GeneralInfoComponent } from '../../components/general-info/general-info';
+
 @IonicPage()
 @Component({
   selector: 'page-room',
@@ -9,11 +14,15 @@ import { Slides, IonicPage, NavController, NavParams } from 'ionic-angular';
 export class RoomPage {
   @ViewChild (Slides) slides: Slides;
 
+  general = DashboardPage;
+  existingLight = ExistingLightComponent;
+  replacement = ReplacementComponent;
+  
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad RoomPage');
+  
   }
 
   goToSlide(index): void {
