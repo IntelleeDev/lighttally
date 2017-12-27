f39c11a64afd98acd1d74b1ac27d23ed8e879ece
mikeysweede <mikeysweede@gmail.com>
2017-12-27 23:39:56 +0100

    Connecting location page

--- src/repository/user-repository.ts
+++ src/repository/user-repository.ts
@@ -5,11 +5,10 @@ import { Repository } from './base';
 import { User } from '../model/user'
 
 export class UserRepository implements Repository<User> {
-  store(data: User): Observable<any> {
-    throw new Error("Method not implemented.");
-  }
   findByFilter(whereFilter: string): Observable<User> {
     throw new Error("Method not implemented.");
   }
-
+  store(data: User): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
 }
