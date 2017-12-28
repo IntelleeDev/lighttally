1636d7a54a78389b64f5be16d3f9eb8a2e6ec4e8
mikeysweede <mikeysweede@gmail.com>
2017-12-28 20:57:02 +0100

    Working fix

--- src/components/bulb-dialog/bulb-dialog.ts
+++ src/components/bulb-dialog/bulb-dialog.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { ViewController } from 'ionic-angular';
 
+import { EXISTING_BULBS } from '../../data/bulbs';
+
 @Component({
   selector: 'bulb-dialog',
   templateUrl: 'bulb-dialog.html'
 })
 export class BulbDialogComponent {
+
+  bulbs: any = EXISTING_BULBS;
 
   constructor(public viewCtrl: ViewController) { }
 
---
+++ src/data/bulbs.ts
@@ -0,0 +1,25 @@
+
+export const EXISTING_BULBS = [
+  {
+    category: 'Incandescent',
+    types: [
+      { name: 'Type - 01', src: '../assets/imgs/a15.png' },
+      { name: 'Type - 02', src: '../assets/imgs/a23.png' },
+      { name: 'Type - 03', src: '../../assets/a10.png' },
+      { name: 'Type - 04', src: '../../assets/a13.png' },
+      { name: 'Type - 05', src: '../../assets/c6.png' },
+      { name: 'Type - 06', src: '../../assets/c11.png' },
+    ]
+  },
+  {
+    category: 'Halogen',
+    types: [
+      { name: 'Type - 01', src: '../../assets/a15.png' },
+      { name: 'Type - 02', src: '../../assets/a23.png' },
+      { name: 'Type - 03', src: '../../assets/a10.png' },
+      { name: 'Type - 04', src: '../../assets/a13.png' },
+      { name: 'Type - 05', src: '../../assets/c6.png' },
+      { name: 'Type - 06', src: '../../assets/c11.png' },
+    ]
+  }  
+]
