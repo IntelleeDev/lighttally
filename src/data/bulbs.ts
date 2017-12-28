ba6b9007a2722a0a1bb7a411bef395bf8e77fe59
mikeysweede <mikeysweede@gmail.com>
2017-12-28 22:02:18 +0100

    Ui complete

--- src/data/bulbs.ts
+++ src/data/bulbs.ts
@@ -5,21 +5,21 @@ export const EXISTING_BULBS = [
     types: [
       { name: 'Type - 01', src: '../assets/imgs/a15.png' },
       { name: 'Type - 02', src: '../assets/imgs/a23.png' },
-      { name: 'Type - 03', src: '../../assets/a10.png' },
-      { name: 'Type - 04', src: '../../assets/a13.png' },
-      { name: 'Type - 05', src: '../../assets/c6.png' },
-      { name: 'Type - 06', src: '../../assets/c11.png' },
+      { name: 'Type - 03', src: '../assets/imgs/b10.png' },
+      { name: 'Type - 04', src: '../assets/imgs/b13.png' },
+      { name: 'Type - 05', src: '../assets/imgs/c6.png' },
+      { name: 'Type - 06', src: '../assets/imgs/c11.png' },
     ]
   },
   {
     category: 'Halogen',
     types: [
-      { name: 'Type - 01', src: '../../assets/a15.png' },
-      { name: 'Type - 02', src: '../../assets/a23.png' },
-      { name: 'Type - 03', src: '../../assets/a10.png' },
-      { name: 'Type - 04', src: '../../assets/a13.png' },
-      { name: 'Type - 05', src: '../../assets/c6.png' },
-      { name: 'Type - 06', src: '../../assets/c11.png' },
+      { name: 'Type - 01', src: '../assets/imgs/a15.png' },
+      { name: 'Type - 02', src: '../assets/imgs/a23.png' },
+      { name: 'Type - 03', src: '../assets/imgs/a10.png' },
+      { name: 'Type - 04', src: '../assets/imgs/a13.png' },
+      { name: 'Type - 05', src: '../assets/imgs/c6.png' },
+      { name: 'Type - 06', src: '../assets/imgs/c11.png' },
     ]
   }  
 ]
