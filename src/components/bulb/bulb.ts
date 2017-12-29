90cd6705c6fe5d08c44aa34d5a483b80366dc044
mikeysweede <mikeysweede@gmail.com>
2017-12-29 12:07:42 +0100

    Reworked changes

--- src/components/bulb/bulb.ts
+++ src/components/bulb/bulb.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ModalController } from 'ionic-angular';
+
+import { BulbDialogComponent } from '../bulb-dialog/bulb-dialog';
 
 @Component({
   selector: 'bulb',
@@ -6,12 +9,23 @@ import { Component } from '@angular/core';
 })
 export class BulbComponent {
 
-  data: Map<string, string>;
+  public readonly data: Array<string> = [];
 
-  constructor() {}
+  constructor(private modalCtrl: ModalController) {}
+
+  public openModal() {
+    let modal = this.modalCtrl.create(BulbDialogComponent);
+    modal.onDidDismiss(data => {
+      this.addItem(data);
+    })
+    modal.present();
+  }
 
   getData(): Array<string> {
     return new Array();
   }
 
+  addItem(value: string) {
+    this.data.push(value);
+  }
 }
