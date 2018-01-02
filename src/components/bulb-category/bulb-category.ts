af6290ea7017b8e84f2a19a68715bfd65669f6f0
mikeysweede <mikeysweede@gmail.com>
2018-01-02 22:44:33 +0100

    Bleeding changes made

--- src/components/bulb-category/bulb-category.ts
+++ src/components/bulb-category/bulb-category.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { ViewController, ModalController } from 'ionic-angular';
 
 import { BULB_CATEGORIES } from '../../data/constants';
+import { BulbDialogComponent } from '../bulb-dialog/bulb-dialog';
 
 @Component({
   selector: 'bulb-category',
@@ -11,8 +13,15 @@ export class BulbCategoryComponent {
   headTitle = 'Bulb Categories';
   public categories: Array<{type: string, src: string}> = [];
 
-  constructor() { 
+  constructor(
+    public viewCtrl: ViewController,
+    private modalCtrl: ModalController) { 
     this.categories = BULB_CATEGORIES;
+  }
+
+  public openBulbDialog() {
+    const modal = this.modalCtrl.create(BulbDialogComponent);
+    modal.present();
   }
 
 }
