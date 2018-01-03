293457b3eae4bc33dbf95f0a4614de638f02309d
mikeysweede <mikeysweede@gmail.com>
2018-01-03 13:47:55 +0100

    Bleeding changes made now

--- src/components/work-time/work-time.ts
+++ src/components/work-time/work-time.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'work-time',
@@ -6,9 +7,10 @@ import { Component } from '@angular/core';
 })
 export class WorkTimeComponent {
 
-  daysOfWeek: Array<any>;
+  public daysOfWeek: Array<any>;
+  public workTimeForm: FormGroup;
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
     this.daysOfWeek = [
       { day: 'Monday', from: 'monFrom', to: 'monTo' },
       { day: 'Tuesday', from: 'tueFrom', to: 'tueTo' },
@@ -16,7 +18,10 @@ export class WorkTimeComponent {
       { day: 'Thursday', from: 'thuFrom', to: 'thuTo' },
       { day: 'Friday', from: 'friFrom', to: 'friTo' },
     ];
+  }
 
+  private createForm() {
+    this.workTimeForm = this.formBuilder.group({});
   }
 
 }
