e97ac84983a10aff01d1e444fce984461bfdc176
mikeysweede <mikeysweede@gmail.com>
2018-01-03 14:11:27 +0100

    Bleeding change newly made

--- src/components/work-time/work-time.ts
+++ src/components/work-time/work-time.ts
@@ -18,17 +18,58 @@ export class WorkTimeComponent {
       { day: 'Thursday', from: 'thuFrom', to: 'thuTo' },
       { day: 'Friday', from: 'friFrom', to: 'friTo' },
     ];
+
     this.createForm();
+    this.workTimeForm.valueChanges.subscribe(this.getData.bind(this));
   }
 
   private createForm() {
     this.workTimeForm = this.formBuilder.group({
-      Monday: this.formBuilder.group({}),
-      Tuesday: this.formBuilder.group({}),
-      Wednesday: this.formBuilder.group({}),
-      Thursday: this.formBuilder.group({}),
-      Friday: this.formBuilder.group({}),
+      Monday: this.formBuilder.group({
+        monFrom: '',
+        monTo: ''
+      }),
+      Tuesday: this.formBuilder.group({
+        tueFrom: '',
+        tueTo: ''
+      }),
+      Wednesday: this.formBuilder.group({
+        wedFrom: '',
+        wedTo: ''
+      }),
+      Thursday: this.formBuilder.group({
+        thuFrom: '',
+        thuTo: ''
+      }),
+      Friday: this.formBuilder.group({
+        friFrom: '',
+        friTo: ''
+      }),
     });
+  }
+
+
+  private computeTotalWorkHours() {
+    const data = this.getData();
+
+    data.reduce(() => {}, 0);
+  }
+
+  private getData(): Array<any> {
+    const formModel = this.workTimeForm.value;
+
+    for (let val of formModel) {
+      alert(val);
+    }
+
+    const data = [];
+    data[0] = { from: formModel.Monday.monFrom, to: formModel.Monday.to };
+    data[1] = { from: formModel.Monday.monFrom, to: formModel.Monday.to };
+    data[2] = { from: formModel.Monday.monFrom, to: formModel.Monday.to };
+    data[3] = { from: formModel.Monday.monFrom, to: formModel.Monday.to };
+    data[4] = { from: formModel.Monday.monFrom, to: formModel.Monday.to };
+
+    return data;
   }
 
 }
