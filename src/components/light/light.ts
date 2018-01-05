74ffeea35ca6b65d432c3ab2cb05584e70077554
mikeysweede <mikeysweede@gmail.com>
2018-01-05 19:45:36 +0100

    Reworked Light component

--- src/components/light/light.ts
+++ src/components/light/light.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ViewController } from 'ionic-angular';
 
 import { WATTAGE_METRICS } from '../../data/constants';
@@ -9,16 +10,20 @@ import { WATTAGE_METRICS } from '../../data/constants';
 })
 export class LightComponent {
   
-  public retroFit: boolean;
-  public wattage: string = '';
-  public metrics: Array<any> = WATTAGE_METRICS[0].wattage;
+  kalvin: number;
+  retroFit: boolean;
+  wrongBulb: boolean;
+  wattage: string = '';
+  metrics: Array<any> = WATTAGE_METRICS[0].wattage;
 
   constructor(private viewCtrl: ViewController) { }
 
   public dismissPopover() {
     this.viewCtrl.dismiss({
+      kavlin: this.kalvin,
       wattage: this.wattage,
-      retrofit: this.retroFit
+      retrofit: this.retroFit,
+      wrongBulb: this.wrongBulb
     });
   }
 
