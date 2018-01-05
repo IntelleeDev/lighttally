2cdf5715ff679d3c211704aabd44b69f83f18717
mikeysweede <mikeysweede@gmail.com>
2018-01-05 23:56:45 +0100

    Stage One Pass

--- src/pages/sign-in/sign-in.ts
+++ src/pages/sign-in/sign-in.ts
@@ -33,19 +33,19 @@ export class SignInPage {
   authenticateUser() {
     this.showSpinner();
 
-    // this.authProvider
-    //     .authenticateUser(this.getData())
-    //     .subscribe(authenticated => {
-    //       if (authenticated) {
-    //         this.hideSpinner();
-    //         this.toHomePage();          
-    //       } else {
-    //         this.hideSpinner();
-    //         this.errorMessage = 'Invalid username or password';
-    //       }
-    //     });
-    this.hideSpinner();
-    this.toHomePage();
+    this.authProvider
+        .authenticateUser(this.getData())
+        .subscribe(authenticated => {
+          if (authenticated) {
+            this.hideSpinner();
+            this.toHomePage();          
+          } else {
+            this.hideSpinner();
+            this.errorMessage = 'Invalid username or password';
+          }
+        });
+    // this.hideSpinner();
+    // this.toHomePage();
   }
 
   toHomePage(): void {
