8427847c1fd03c433173f4e370526330ed69534a
mikeysweede <mikeysweede@gmail.com>
2018-01-07 13:47:01 +0100

    Fixed replacement bulb

--- src/components/replacement/replacement.ts
+++ src/components/replacement/replacement.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Replacement } from '../../model/replacement';
+import { BulbSelectionComponent } from '../bulb-selection/bulb-selection';
 
 @Component({
   selector: 'replacement',
   templateUrl: 'replacement.html'
 })
 export class ReplacementComponent {
-  
+  @ViewChild(BulbSelectionComponent) bulbSelection: BulbSelectionComponent;
+
   title = 'Replacement Bulb';
   replacementForm: FormGroup;
   
@@ -24,10 +26,14 @@ export class ReplacementComponent {
 
   getData(): Replacement {
     const formModel = this.replacementForm.value;
+    const replacementBulb = this.bulbSelection.getSelectedBulb();
 
     const replacement: Replacement = {
+      replacementBulb, 
       specialNotes: formModel.specialNotes
     }
     return replacement;
   }
+
+  addBulb() { }
 }
