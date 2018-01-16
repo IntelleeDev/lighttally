15ec4dfccde4a2cbde155dc714bb41d9b81ea96d
mikeysweede <mikeysweede@gmail.com>
2018-01-16 18:39:38 +0100

    Added Location data

--- src/providers/pdf/doc-printer/basic-document-printer.ts
+++ src/providers/pdf/doc-printer/basic-document-printer.ts
@@ -14,18 +14,34 @@ export class BasicDocumentPrinter implements PrettyPrinter {
     content.push({ text: 'Location Details', bold: true });
     content.push({
       table: {
-        width: [ 'auto', 'auto', 'auto', 'auto', 'auto', 'auto', 'auto' ],
+        headerRows: 2,
+        width: [ 'auto', 50, '*', '*', '*', '*', '*' ],
         body: [
           [
-            { text: 'Location', style: 'tableHeader', alignment: 'center', colSpan: 6 },
+            { text: 'Location', style: 'tableHeader', alignment: 'center', colSpan: 7 },
            {}, {}, {}, {}, {}, {}
+          ],
+          [
+            { text: 'Address', style: 'tableHeader', alignment: 'center' },
+            { text: 'Business Name', style: 'tableHeader', alignment: 'center' },
+            { text: 'Account No', style: 'tableHeader', alignment: 'center' },
+            { text: 'Energy Company', style: 'tableHeader', alignment: 'center' },
+            { text: 'KWH Filed', style: 'tableHeader', alignment: 'center' },
+            { text: 'Square Ft', style: 'tableHeader', alignment: 'center' },
+            { text: 'Working Hrs', style: 'tableHeader', alignment: 'center' },
+          ],
+          [
+            `${docData.location.address}`,
+            `${docData.location.businessName}`,
+            `${docData.location.accountNumber}`,
+            `${docData.location.energyCompany}`,
+            `${docData.location.khwFiled}`,
+            `${docData.location.squareFootage}`,
+            `${docData.location.workingHours}`,
           ]
         ]
       }
     });
-    content.push({ text: `Evaluation for ${docData.location.businessName}`})
-    content.push({ text: 'Location Data'});
-    content.push({ text: ''})
 
     const styles = {
       header: {
@@ -35,8 +51,11 @@ export class BasicDocumentPrinter implements PrettyPrinter {
       },
       tableHeader: {
         bold: true,
-        fontSize: 14,
+        fontSize: 13,
         color: 'black'
+      },
+      tableContent: {
+        fontSize: 13
       }
     }
     return { content, styles } as any ;
