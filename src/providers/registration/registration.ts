414062e5c483ce2b7f16861ebadb3eab89eaa958
mikeysweede <mikeysweede@gmail.com>
2018-01-18 12:30:27 +0100

    Debugging registration

--- src/providers/registration/registration.ts
+++ src/providers/registration/registration.ts
@@ -16,12 +16,6 @@ export class RegistrationProvider {
     private userRepository: UserRepository) { }
 
   registerUser(user: User): Promise<any> {
-
-    // To register the user 
-    // Get the user's credentials
-    // Store the user's email and password
-    // Update the users' name and photoUrl
-    // store extra attributes in the users' table in the firestore database
     return this.userRepository
                .store(user)
                .then(id => {
